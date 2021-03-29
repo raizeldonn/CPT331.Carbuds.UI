9a6d15390da97a9556df0142786318dcf7978369
Mitchell Young <s3634235@student.rmit.edu.au>
2021-03-29 18:25:48 +1100

    Added register UI

---
+++ src/app/components/auth/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  public registerForm: FormGroup;
+
+  constructor(private _authService: AuthService, private _toastr: ToastrService, private _router: Router) {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+			emailAddress: new FormControl('', Validators.required),
+			password: new FormControl('', Validators.required)
+		});
+  }
+
+  ngOnInit(): void {
+  }
+
+  public onLoginClick(){
+    this._router.navigateByUrl('login');
+  }
+
+  public async onSubmitRegisterForm() {
+
+  }
+}
