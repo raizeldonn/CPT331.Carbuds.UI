d0b26d1f4e6860a1c5a730632904b2a91edb9c6a
Rolando <chancellor.rolando@gmail.com>
2021-04-05 21:51:21 +1000

    fix issues with api integration (model parsing)

--- src/app/components/admin/add-car/add-car.component.ts
+++ src/app/components/admin/add-car/add-car.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CarService } from 'src/app/services/car.service';
 import { ToastrService } from 'ngx-toastr';
+import { Car } from 'src/app/models/car/car.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-add-car',
@@ -25,7 +27,7 @@ export class AddCarComponent implements OnInit {
       carSeats: new FormControl('', Validators.required),
       carPriceHour: new FormControl('', Validators.required),
       carPriceDay: new FormControl('', Validators.required),
-      carImage: new FormControl('', Validators.required),
+      carImage: new FormControl(''),
       carStatus: new FormControl('', Validators.required)
 		});
   }
@@ -34,24 +36,30 @@ export class AddCarComponent implements OnInit {
   }
 
   public async onSubmitAddCarForm() {
-    
+
     if(this.addCarForm.valid){
       try{
+
+        let carActive: boolean = this.addCarForm.value['carStatus'] == 'Active' ? true : false;
         
-        let addCarResponse = await this._carService.addCar(
-          this.addCarForm.value['carMake'],
-          this.addCarForm.value['carModel'],
-          this.addCarForm.value['carYear'],
-          this.addCarForm.value['carTransmission'],
-          this.addCarForm.value['carKilometers'],
-          this.addCarForm.value['carLocation'],
-          this.addCarForm.value['carBody'],
-          this.addCarForm.value['carDoors'],
-          this.addCarForm.value['carSeats'],
-          this.addCarForm.value['carPriceHour'],
-          this.addCarForm.value['carPriceDay'],
-          this.addCarForm.value['carImage'],
-          this.addCarForm.value['carStatus']);
+        let carToAdd: Car = {
+          uuid: uuidv4(),
+          make: this.addCarForm.value['carMake'],
+          model: this.addCarForm.value['carModel'],
+          year: this.addCarForm.value['carYear'],
+          transmission: this.addCarForm.value['carTransmission'],
+          kilometers: this.addCarForm.value['carKilometers'],
+          body: this.addCarForm.value['carBody'],
+          location: this.addCarForm.value['carLocation'],
+          doors: this.addCarForm.value['carDoors'],
+          seats: this.addCarForm.value['carSeats'],
+          priceHour: this.addCarForm.value['carPriceHour'],
+          priceDay: this.addCarForm.value['carPriceDay'],
+          imageId: '',
+          isActive: carActive
+        };
+        
+        let addCarResponse = await this._carService.addEditCar(carToAdd);
 
           if (addCarResponse.success) {				
             console.log('success!')
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -13,33 +13,12 @@ import { environment } from 'src/environments/environment';
 
 export class CarService {
 
-    public car?: Car;
-
     constructor(private _http: HttpClient){}
 
-    public async addCar(make: string, model: string, year: number, transmission: string,
-        kilometers: number, body: string, location: string, doors: number,
-        seats: number, priceHour: number, priceDay: number, imageId: string, isActive: boolean): Promise<PostAddUpdateCarResponse>{
-    
-            this.car = {
-                uuid: "eca78f2a-f7e6-47d2-b570-171d93b5e0b5",
-                make: make,
-                model: model,
-                year: year,
-                transmission: transmission,
-                kilometers: kilometers,
-                body: body,
-                location: location,
-                doors: doors,
-                seats: seats,
-                priceHour: priceHour,
-                priceDay: priceDay,
-                imageId: imageId,
-                isActive: isActive
-            };
+    public async addEditCar(record: Car): Promise<PostAddUpdateCarResponse>{
 
         let request: PostAddUpdateCarRequest = {
-          car: this.car
+          car: record
         }
     
         let response = await this._http.post<PostAddUpdateCarResponse>(`${environment.apiBaseUrl}/api/cars`, request).toPromise();    
