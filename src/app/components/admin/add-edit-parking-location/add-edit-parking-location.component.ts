f25386f48b9cf8165dc7601d248834a6f27a5f5a
Rolando <chancellor.rolando@gmail.com>
2021-04-12 22:14:00 +1000

    got clickable map marker working

--- src/app/components/admin/add-edit-parking-location/add-edit-parking-location.component.ts
+++ src/app/components/admin/add-edit-parking-location/add-edit-parking-location.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LngLat, MapMouseEvent } from 'mapbox-gl';
 
 @Component({
   selector: 'app-add-edit-parking-location',
@@ -7,13 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditParkingLocationComponent implements OnInit {
 
-  constructor() { }
+  public mapMarkerLocation: LngLat = new LngLat(144.944192, -37.816675);
+
+  public addParkingForm: FormGroup;
+
+  constructor() {
+    this.addParkingForm = new FormGroup({
+      locationName: new FormControl('', Validators.required)
+    });
+  } 
 
   ngOnInit(): void {
+    
   }
 
   public onMarkerClick(){
     alert('Foo')
+  }
+
+  public onMapClick(event: MapMouseEvent){
+    
+    let clickedLocation = event.lngLat;
+
+    if(clickedLocation){
+      console.log(clickedLocation);
+      this.mapMarkerLocation = new LngLat(clickedLocation.lng, clickedLocation.lat);
+    }
+
   }
 
 }
