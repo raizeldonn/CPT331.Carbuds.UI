f2c73cbf0f59eb76a6119e4325770bc47c4783f6
raizel <s3690975@student.rmit.edu.au>
2021-04-12 10:28:19 +0300

    user profile info from api
    still need to get the email from the current logged in user. atm its a hardcoded email

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user/user.model';
+import { UserService } from 'src/app/services/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  //dont know if this is correct thing to do
+  //did it for now to get error to go away
+  // how to fix??
+  public user!: User;
+
+  constructor(private _userService: UserService, private _toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.getUserData();
+  }
+
+  public async getUserData(){
+    try {
+
+      let userResponse = await this._userService.getUser();
+      if (userResponse.success) {
+        this.user = userResponse.user;
+      }
+      else {
+        this._toastr.error(userResponse.errorMessage, 'Unable to Get User');
+      }
+    } catch (e) {
+      this._toastr.error(e, 'Unable to Get User');
+    }
   }
 
 }
---
+++ src/app/contracts/user/get.user.response.model.ts
@@ -0,0 +1,6 @@
+import { User } from "src/app/models/user/user.model";
+import { BaseResponse } from "../base.response.model";
+
+export interface GetUserResponse extends BaseResponse {
+    user: User;
+}
