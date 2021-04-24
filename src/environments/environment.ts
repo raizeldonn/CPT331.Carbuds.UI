634f2998358947ac2a35507fbeda1448cda26ac6
Rolando <chancellor.rolando@gmail.com>
2021-04-24 12:37:09 +1000

    update environment variables for API url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://72k0egrzf1.execute-api.ap-southeast-2.amazonaws.com/Dev',
   cognitoClientId: '1btr18nik21i9fn341cou475tg'
 };
 
