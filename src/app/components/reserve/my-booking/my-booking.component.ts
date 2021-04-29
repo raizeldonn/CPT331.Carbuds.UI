34b1f101b513eb933b4b3325853eaf8a9194501d
raizel <s3690975@student.rmit.edu.au>
2021-04-29 11:55:53 +0300

    my-booking component styling

--- src/app/components/reserve/my-booking/my-booking.component.ts
+++ src/app/components/reserve/my-booking/my-booking.component.ts
@@ -13,4 +13,20 @@ export class MyBookingComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public onCancelClick(){
+    this._activeModal.dismiss(null);
+    //also need to cancel the booking
+    //ie remove from db
+  }
+
+  public onUnlockCarClick(){
+    this._activeModal.dismiss(null);
+    // give a code to unlock the car
+  }
+
+  public viewOnMapClick(){
+    this._activeModal.dismiss(null);
+    //roly - show the user the car location on a map 
+  }
+
 }
